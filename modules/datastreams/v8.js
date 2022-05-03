117de7479f49d5f61ecf7339ad55a6b684055771
Ignacio Sans <dbleyenda@gmail.com>
2022-05-03 12:33:12 -0300

    ajustes vistas de tipo escritura

--- modules/datastreams/v8.js
+++ modules/datastreams/v8.js
@@ -624,16 +624,16 @@ function renderV8CellTable(str, fType) {
         case "LINK":
             return '<table class="text"><tr><td>' +  str + '</td></tr></table>';
         case "ERROR":
-            return '<table class="null"><tr><td> ' + (Configuration.language == 'es') ? 'No se encontraron datos' : 'No data found' + '. <span>'
-                + (Configuration.language == 'es') ? 'Por favor' : 'Please' + ' <a id="id_retryButton" title="'
-                + (Configuration.language == 'es') ? 'Inténtalo Nuevamente' : 'Try Again' + '">'
-                + (Configuration.language == 'es') ? 'inténtalo Nuevamente' : 'try again' + '</a>.</span></td></tr></table>';
+            return '<table class="null"><tr><td> ' + ( (Configuration.language == 'es') ? 'No se encontraron datos' : 'No data found' ) + '. <span>'
+                + ( (Configuration.language == 'es') ? 'Por favor' : 'Please' ) + ' <a id="id_retryButton" title="'
+                + ( (Configuration.language == 'es') ? 'Inténtalo Nuevamente' : 'Try Again' ) + '">'
+                + ( (Configuration.language == 'es') ? 'inténtalo Nuevamente' : 'try again' ) + '</a>.</span></td></tr></table>';
         case "CONFIRM-WRITE-DATASET":
-            return '<table class="text"><tr><td> ' + (Configuration.language == 'es') ? 'Usted se encuentra por ejecutar un origen que puede cambiar datos.' : 'You are about to run an origin that can affect the data.'
-                + '. <span><a id="id_retryButton" title="' + (Configuration.language == 'es') ? 'Continuar de todas formas' : 'Continue anyway' + '">'
-                + (Configuration.language == 'es') ? 'Continuar de todas formas' : 'Continue anyway' + '</a></span></td></tr></table>';
+            return '<table class="text"><tr><td> ' + ( (Configuration.language == 'es') ? 'Usted se encuentra por ejecutar un origen que puede cambiar datos.' : 'You are about to run an origin that can affect the data.' )
+                + '. <span><a id="id_retryButton" title="' + ( (Configuration.language == 'es') ? 'Continuar de todas formas' : 'Continue anyway' ) + '">'
+                + ( (Configuration.language == 'es') ? 'Continuar de todas formas' : 'Continue anyway' )  + '</a></span></td></tr></table>';
         case "CONFIRM-WRITE-DATASET-PRO":
-            return '<table class="text"><tr><td> ' + (Configuration.language == 'es') ? 'Las vistas de tipo escritura que pertenecen al entorno de producción no se pueden previsualizar en el espacio de trabajo.' : 'Write-type Data Views that belong to the production environment cannot be previewed in the workspace.' + '</td></tr></table>';
+            return '<table class="text"><tr><td> ' + ( (Configuration.language == 'es') ? 'Las vistas de tipo escritura que pertenecen al entorno de producción no se pueden previsualizar en el espacio de trabajo.' : 'Write-type Data Views that belong to the production environment cannot be previewed in the workspace.' ) + '</td></tr></table>';
     }
     return ''
 }
